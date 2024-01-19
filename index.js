1973f53b6565b93c4d1a4b4bfd871d6513587544
Victor Chan <victorchanhc@gmail.com>
2024-01-19 00:04:17 +0000

    created change lesson page

---
+++ index.js
@@ -0,0 +1,15 @@
+// const editlesson = document.querySelector('.editInformation-content')
+
+// document.querySelector('.editButton')
+//     .addEventListener('click', function () {
+//         editlesson.innerHTML += `<div class="dropdown">
+//         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//           Dropdown button
+//         </button>
+//         <ul class="dropdown-menu">
+//           <li><a class="dropdown-item" href="#">Action</a></li>
+//           <li><a class="dropdown-item" href="#">Another action</a></li>
+//           <li><a class="dropdown-item" href="#">Something else here</a></li>
+//         </ul>
+//       </div>`
+//     });
