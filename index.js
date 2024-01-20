ff0785e659dc6d65325d8c8caa035ff747e452a8
Victor Chan <victorchanhc@gmail.com>
2024-01-20 01:14:04 +0000

    Created Lesson page

--- index.js
+++ index.js
@@ -12,4 +12,29 @@
 //           <li><a class="dropdown-item" href="#">Something else here</a></li>
 //         </ul>
 //       </div>`
-//     });
+// //     });
+// const lessonContainer = document.querySelector('#allLessonContainer')
+
+// document.querySelector('#joinButton')
+//     .addEventListener('click', function () {
+//         h
+        // lessonContainer.innerHTML = '';
+        // lessonContainer.innerHTML +=
+        //     `<div class="row">
+        //         <div class="col-md-4 d-flex">
+        //             <a class="btn btn-outline-dark btn-sm" href="lesson.html" id="go-back">Go back!</a>
+        //         </div>
+        //         <div class="col-md-4 d-flex">
+        //             <h1 class="titleBar welcome mb-4 text-center">Lesson</h1>
+        //         </div>
+        //     </div>
+        //     <div class="card card-body d-flex justify-content-between">
+        //         <div class="lessonInformation-content">
+        //             <!-- import the lesson information -->
+        //             <h5 class="lessonName">星期日 晚上 荃灣</h5>
+        //             <div class="lessonDate">日期 : 7/1 14/1 21/1 28/1</div>
+        //             <div class="lessonTime">時間 : 11:00 - 13:00</div>
+        //             <div class="lessonVenus">地點 : 城門谷五人場</div>
+        //         </div>
+        //     </div>`;
+    // })
